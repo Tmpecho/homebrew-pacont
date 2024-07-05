e02c3932085d73230f6f3c9d3e3f0255a4d8919a
Johannes Aamot-Skeidsvoll <82368148+Tmpecho@users.noreply.github.com>
2024-07-05 20:56:27 +0200

    Create pacont.rb

---
+++ Formula/pacont.rb
@@ -0,0 +1,11 @@
+class PrintFiles < Formula
+  desc "A Rust CLI program to print file names and contents"
+  homepage "https://github.com/Tmpecho/pacont"
+  url "https://github.com/Tmpecho/pacont/releases/download/v0.1.1/pacont"
+  sha256 "821c2f50754f4b45aecd976db76d0c5b20c33beb6c86a57ed8eb22cc44ebeaff"
+  version "0.1.1"
+
+  def install
+    bin.install "pacont"
+  end
+end
