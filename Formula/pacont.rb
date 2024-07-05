cd3d8aff4bb1e8ce8b1a5efefbfe9334880f43fa
Johannes Aamot-Skeidsvoll <82368148+Tmpecho@users.noreply.github.com>
2024-07-06 01:33:11 +0200

    Update release

--- Formula/pacont.rb
+++ Formula/pacont.rb
@@ -1,9 +1,9 @@
 class Pacont < Formula
   desc "A Rust CLI program to print file names and contents"
   homepage "https://github.com/Tmpecho/pacont"
-  url "https://github.com/Tmpecho/pacont/releases/download/v0.2.0/pacont"
-  sha256 "b0e9f8e1a95ccec53f47b6d53b11b194b5362673da525f51a73b8330e09ee632"
-  version "0.2.0"
+  url "https://github.com/Tmpecho/pacont/releases/download/v0.3.0/pacont"
+  sha256 "25d3e60dc768208dd87a1a3b9f1ba36b69ba9a8234c10e9547922b88f4cc246f"
+  version "0.3.0"
 
   def install
     bin.install "pacont"
