cad62c188caf77d069ce807b0c2ef0204d3ace6c
Johannes Aamot-Skeidsvoll <82368148+Tmpecho@users.noreply.github.com>
2024-07-05 23:13:59 +0200

    Update pacont.rb verion number

--- Formula/pacont.rb
+++ Formula/pacont.rb
@@ -3,7 +3,7 @@ class Pacont < Formula
   homepage "https://github.com/Tmpecho/pacont"
   url "https://github.com/Tmpecho/pacont/releases/download/v0.1.1/pacont"
   sha256 "821c2f50754f4b45aecd976db76d0c5b20c33beb6c86a57ed8eb22cc44ebeaff"
-  version "0.1.1"
+  version "0.2.0"
 
   def install
     bin.install "pacont"
